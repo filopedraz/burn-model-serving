fec339db434324c1301cfb4db6f9d79e88667a42
Filippo Pedrazzini <filippo.pedrazzini@joandko.io>
2023-09-14 10:51:35 +0200

    removing torch dependencies

--- src/bin/predict.rs
+++ src/bin/predict.rs
@@ -2,11 +2,11 @@ use burn::tensor::backend::ADBackend;
 
 use burn_model_serving::AgNewsDataset;
 
-#[cfg(not(feature = "f16"))]
+use burn::autodiff::ADBackendDecorator;
+use burn::backend::wgpu::{AutoGraphicsApi, WgpuBackend, WgpuDevice};
+
 #[allow(dead_code)]
 type ElemType = f32;
-#[cfg(feature = "f16")]
-type ElemType = burn::tensor::f16;
 
 pub fn launch<B: ADBackend>(device: B::Device) {
     burn_model_serving::inference::infer::<B, AgNewsDataset>(
@@ -21,78 +21,8 @@ pub fn launch<B: ADBackend>(device: B::Device) {
     );
 }
 
-#[cfg(any(
-    feature = "ndarray",
-    feature = "ndarray-blas-netlib",
-    feature = "ndarray-blas-openblas",
-    feature = "ndarray-blas-accelerate",
-))]
-mod ndarray {
-    use burn::autodiff::ADBackendDecorator;
-    use burn::backend::ndarray::{NdArrayBackend, NdArrayDevice};
-
-    use crate::{launch, ElemType};
-
-    pub fn run() {
-        launch::<ADBackendDecorator<NdArrayBackend<ElemType>>>(NdArrayDevice::Cpu);
-    }
-}
-
-#[cfg(feature = "tch-gpu")]
-mod tch_gpu {
-    use burn::autodiff::ADBackendDecorator;
-    use burn::backend::tch::{TchBackend, TchDevice};
-
-    use crate::{launch, ElemType};
-
-    pub fn run() {
-        #[cfg(not(target_os = "macos"))]
-        let device = TchDevice::Cuda(0);
-        #[cfg(target_os = "macos")]
-        let device = TchDevice::Mps;
-
-        launch::<ADBackendDecorator<TchBackend<ElemType>>>(device);
-    }
-}
-
-#[cfg(feature = "tch-cpu")]
-mod tch_cpu {
-    use burn::autodiff::ADBackendDecorator;
-    use burn::backend::tch::{TchBackend, TchDevice};
-
-    use crate::{launch, ElemType};
-
-    pub fn run() {
-        launch::<ADBackendDecorator<TchBackend<ElemType>>>(TchDevice::Cpu);
-    }
-}
-
-#[cfg(feature = "wgpu")]
-mod wgpu {
-    use burn::autodiff::ADBackendDecorator;
-    use burn::backend::wgpu::{AutoGraphicsApi, WgpuBackend, WgpuDevice};
-
-    use crate::{launch, ElemType};
-
-    pub fn run() {
-        launch::<ADBackendDecorator<WgpuBackend<AutoGraphicsApi, ElemType, i32>>>(
-            WgpuDevice::default(),
-        );
-    }
-}
-
 fn main() {
-    #[cfg(any(
-        feature = "ndarray",
-        feature = "ndarray-blas-netlib",
-        feature = "ndarray-blas-openblas",
-        feature = "ndarray-blas-accelerate",
-    ))]
-    ndarray::run();
-    #[cfg(feature = "tch-gpu")]
-    tch_gpu::run();
-    #[cfg(feature = "tch-cpu")]
-    tch_cpu::run();
-    #[cfg(feature = "wgpu")]
-    wgpu::run();
+    launch::<ADBackendDecorator<WgpuBackend<AutoGraphicsApi, ElemType, i32>>>(
+        WgpuDevice::default(),
+    );
 }
